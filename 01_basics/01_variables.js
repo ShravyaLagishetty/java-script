91bc752d0b4d776726ecf157619e87fd144bd19c
Shravya <160641554+ShravyaLagishetty@users.noreply.github.com>
2025-07-13 16:46:52 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId="145623"
+let accountName="shravya"
+var accountPassword="12345"
+accountCity="Hyderabad"
+
+//accountId="23456"
+//prefer not to use var due to block scope and functional scope
+
+console.table([accountId,accountName,accountPassword,accountCity])
