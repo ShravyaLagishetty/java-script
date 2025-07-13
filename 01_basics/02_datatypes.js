6a5cfee8a931f242382ee4db198aedffbf4d085b
Shravya <160641554+ShravyaLagishetty@users.noreply.github.com>
2025-07-13 16:46:35 +0000

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version 
+let name="Shravya"
+let age="20"
+
+console.log(typeof(age)) //type of variable
+
+//convert into number
+let valueInNumber=Number(age)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+//"33"->33
+//"33abc"->NaN
+//true->1
+//false->0
+
+//convert into boolean
+let isLoggedIn="shravya"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1->true
+//""->false
+//" "->true
+//"shravya"->true
+
+let num=15
+let stringNumber=String(num)
+console.log(typeof stringNumber);
+
